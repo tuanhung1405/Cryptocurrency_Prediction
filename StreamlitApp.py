ce229de42ce179395c831a6023f7aaa10a0b711a
tuanhung1405 <120279152+tuanhung1405@users.noreply.github.com>
2022-12-11 14:36:48 +0700

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -39,13 +39,6 @@ div.fullScreenFrame > div {
 #Title
 original_title = '<p style="text-align: center; color:#3498DB; text-shadow: 2px 2px 4px #000000; font-size: 60px;">Crypto-Currency Price Prediction using ARIMA Model</p>'
 st.markdown(original_title, unsafe_allow_html=True)
-
-st.markdown("This is a project of **Nguyen Tuan Hung** from **UEL** that aims to build a web application to forecast the trend of coin prices using the Arima model - a fairly famous model in time series forecasting.")
-st.write("The data was crawled from API of Coinbase (https://api.pro.coinbase.com). You can use the model however you want, but you carry the risk for your actions.") 
-
-background = Image.open("Nguyễn Tuấn Hưng_ Ảnh chân dung.png")
-col1, col2, col3 = st.columns([0.7, 1, 0.7])
-col2.image(background, use_column_width=True)
     
 st.write('---')
 tup, coinname = ApiGetData.getListCoins()
