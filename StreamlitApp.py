cc6b69629f09d2f21955a31d59830172bb27fdaa
tuanhung1405 <120279152+tuanhung1405@users.noreply.github.com>
2022-12-14 11:31:10 +0700

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -24,7 +24,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href=https://github.com/hunter1405?tab=repositories" target="_blank">Portfolio</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://www.facebook.com/hunter.dasick/" target="_blank">Facebook</a>
